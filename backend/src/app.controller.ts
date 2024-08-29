239e5f23ac6de4c95e85dba3ecc754e7f0092993
siuma1109 <matsunkit1109@gmail.com>
2024-08-29 22:20:04 +0800

    Remove hello function and profile get updated user details to return

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,19 +1,19 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { UsersService } from './user/users.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  constructor(
+    private readonly appService: AppService,
+    private usersService: UsersService,
+  ) {}
 
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
-    return req.user;
+    const user = req.user;
+    return this.usersService.findById(user.id);
   }
 }
--- backend/src/user/users.controller.ts
+++ backend/src/user/users.controller.ts
@@ -1,11 +1,4 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  Put,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Get, Param, Put, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
